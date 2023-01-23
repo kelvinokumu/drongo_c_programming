b53b331428c5ca6e9bdfad296977d1a4f50b401e
kelvinokumu <oumakelvinokumu.gmail.com>
2023-01-23 08:21:40 +0300

    added strings

---
+++ 01_basics/strings.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+gets();
+
+int main()
+{   
+    // array to store string taken as input
+    char sentence[30];
+    
+    // take user input
+    printf("Enter any sentence: ");
+    
+    // use the gets method
+    gets(sentence);
+    
+    // printing the input value
+    printf("You entered: %s.", sentence);
+    
+    return 0;
+}
