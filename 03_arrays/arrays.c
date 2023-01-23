00478cefd2f9573ee6f324c995bafaa0d649b4c1
kelvinokumu <oumakelvinokumu.gmail.com>
2023-01-23 09:37:49 +0300

    added more code on arrays

--- 03_arrays/arrays.c
+++ 03_arrays/arrays.c
@@ -1,34 +1,5 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int farrays();
-
-void main()
-{
-    farrays();
+void main(){
+    printf("Arrays \n");
 }
-
-int farrays()
-{
-    int max;
-
-    printf("How many inputs do you need \n");
-    scanf("%i", &max);
-    int array_numbers[max];
-    // receiving inputs
-    for (int i = 0; i < max; i++)
-    {
-        int number;
-        scanf("%i", &number);
-        array_numbers[i] = number;
-        printf("The values at index %i is %i \n", i, number);
-    }
-
-    printf("Printing the values \n");
-    for (int i = 0; i < max; i++)
-    {
-        printf("%i \n", array_numbers[i]);
-    }
-}
-
-
-
