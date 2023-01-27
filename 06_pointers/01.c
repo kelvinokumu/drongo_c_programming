1a3ae4f3ad4f7423c148f1b2f5efbd244f5b4853
kelvinokumu <oumakelvinokumu.gmail.com>
2023-01-27 15:44:47 +0300

    made changes on pointers

--- 06_pointers/01.c
+++ 06_pointers/01.c
@@ -6,12 +6,13 @@ void main(){
     // int * n2;
 
     int a = 45;
-    int *y = &a;
+    int *y; // declare variable y as a pointer
+    *y = &a;
 
     printf("%i \n", a);
     printf("%i \n", *y);
 
-    *y = 75;
+    *y = 75; // change the address of to point to 75
 
     printf("%i \n", a);
     printf("%i \n", *y);
---
+++ 06_pointers/02.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+void main(){
+    int a = 14;
+    int b = 17;
+    b = a;
+
+
+    printf("%i \n", a);
+    printf("%i \n", b);
+}
