d2556cb0dd86b7bf4dcef9bd325139345a63e7d0
kelvinokumu <oumakelvinokumu.gmail.com>
2023-02-01 15:19:28 +0300

    strings

--- 05_strings/01.c
+++ 05_strings/01.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
-#include <stdlib.h>
+// #include <stdlib.h>
 int main()
 {
     char ch;
+
     printf("Enter a character: ");
-    ch = getchar();
+    ch = getchar(); // getting one character from the keyboard
+
     printf("You have entered a character: ");
-    putchar(ch);
+    putchar(ch); // Displaying one character
+
     printf("\n");
     return 0;
 }
--- 05_strings/02.c
+++ 05_strings/02.c
@@ -5,15 +5,18 @@ int main()
     char ch;
     char s[10];
     int i = 0;
+    
     printf("Enter a number or string\n");
     while ((ch = getchar()) != '\n')
     {
         s[i] = ch;
         i++;
     }
+
     printf("Original Number/String: %s\n", s);
+    
     printf("Reverse Number/String: ");
-    while (--i >= 0)
+    while (0 <= i--)
     {
         putchar(s[i]);
     }
