4d03c91c70939990ba6318dbd980b7bfeb656506
kelvinokumu <oumakelvinokumu.gmail.com>
2023-02-01 13:59:32 +0300

    files

--- 08_files/02.c
+++ 08_files/02.c
@@ -15,7 +15,7 @@ int main( )
 
 	// Open the existing file GfgTest.c using fopen()
 	// in read mode using "r" attribute
-	filePointer = fopen("GfgTest.c", "r") ;
+	filePointer = fopen("drongo.c", "r") ;
 	
 	// Check if this filePointer is null
 	// which maybe if the file does not exist
