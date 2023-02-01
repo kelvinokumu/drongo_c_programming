8ad4ef31f260306dc8c8b3a0fa8b1494918dbc0b
kelvinokumu <oumakelvinokumu.gmail.com>
2023-02-01 14:39:13 +0300

    strings

--- 05_strings/03.c
+++ 05_strings/03.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
-#include <stdlib.h>
+// #include <stdlib.h>
 
 int main()
 {
-    char str[81];
+    char str[30];
+
     printf("Enter a line of text:\n");
+    
     fgets(str, sizeof(str), stdin);
     printf("You entered: ");
     puts(str);
