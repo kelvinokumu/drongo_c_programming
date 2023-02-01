794f7975179b6f00b2f2873a33ca1951fd264b77
kelvinokumu <oumakelvinokumu.gmail.com>
2023-02-01 11:19:40 +0300

    return type

--- 03_functions/02.c
+++ 03_functions/02.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
 
-int calculation();
+int calculation(int num1, int num2);
 
 void main(){
-    int v1, v2;
+    int v1, v2, calc;
     printf("Enter first number \n");
     scanf("%i", &v1);
     printf("Enter second number \n");
     scanf("%i", &v2);
-// 
-    int kelvin = calculation(v1,v2);
-    printf("The answer is %i \n", kelvin);
+
+    calc = calculation(v1, v2);
+    printf("The answer is %i \n", calc);
 }
 
 int calculation(int num1, int num2){
