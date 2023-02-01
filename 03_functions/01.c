140fd41456637829fe2fc954e202ade893788f23
kelvinokumu <oumakelvinokumu.gmail.com>
2023-02-01 11:19:24 +0300

    functions

--- 03_functions/01.c
+++ 03_functions/01.c
@@ -1,14 +1,24 @@
 #include <stdio.h>
 
 int sum();
+int sum2(int n1, int n2);
 
 void main()
 {
     printf("Functions \n");
-    sum(18, 25);
+    sum();
+    sum2(18, 25);
 }
 
-int sum(int n1, int n2)
+int sum()
+{
+    int n1 = 300;
+    int n2 = 12;
+    int sum = n1 + n2;
+    printf("The sum is %i \n", sum);
+}
+
+int sum2(int n1, int n2)
 {
     // int n1 = 9;
     // int n2 = 12;
