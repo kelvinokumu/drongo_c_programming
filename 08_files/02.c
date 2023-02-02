3719bd75a3e47a46dfac7a25231795eba1f762d5
kelvinokumu <oumakelvinokumu.gmail.com>
2023-02-02 14:32:53 +0300

    files

--- 08_files/02.c
+++ 08_files/02.c
@@ -1,47 +1,47 @@
 // C program to Open a File,
 // Read from it, And Close the File
 
-# include <stdio.h>
-# include <string.h>
+#include <stdio.h>
+#include <string.h>
 
-int main( )
+int main()
 {
 
 	// Declare the file pointer
-	FILE *filePointer ;
-	
+	FILE *filePointer;
+
 	// Declare the variable for the data to be read from file
 	char dataToBeRead[50];
 
 	// Open the existing file GfgTest.c using fopen()
 	// in read mode using "r" attribute
-	filePointer = fopen("drongo.c", "r") ;
-	
+	filePointer = fopen("drongo.c", "r");
+
 	// Check if this filePointer is null
 	// which maybe if the file does not exist
-	if ( filePointer == NULL )
+	if (filePointer == NULL)
 	{
-		printf( "drongo.c file failed to open." ) ;
+		printf("drongo.c file failed to open.");
 	}
 	else
 	{
-		
-		printf("The file is now opened.\n") ;
-		
+
+		printf("The file is now opened.\n");
+
 		// Read the dataToBeRead from the file
 		// using fgets() method
-		while( fgets ( dataToBeRead, 50, filePointer ) != NULL )
+		while (fgets(dataToBeRead, 50, filePointer) != NULL)
 		{
-		
+
 			// Print the dataToBeRead
-			printf( "%s" , dataToBeRead ) ;
+			printf("%s", dataToBeRead);
 		}
-		
+
 		// Closing the file using fclose()
-		fclose(filePointer) ;
-		
-		printf("Data successfully read from file GfgTest.c\n");
-		printf("The file is now closed.") ;
+		fclose(filePointer);
+
+		printf("Data successfully read from file drongo.c\n");
+		printf("The file is now closed.");
 	}
-	return 0;	
+	return 0;
 }
