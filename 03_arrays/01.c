df339b77ee3bc2bb31f2a839ca9ef731fffe7d6d
kelvinokumu <oumakelvinokumu.gmail.com>
2023-02-02 17:57:16 +0300

    arrays 1D, 2D, 3D

---
+++ 03_arrays/01.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void main()
+{
+    printf("Arrays \n");
+    int nums[5] = {40, 51, 62, 73, 74};
+
+    // printf("%i \n",nums[0]);
+    // printf("%i \n",nums[1]);
+    // printf("%i \n",nums[2]);
+    // printf("%i \n",nums[3]);
+    // printf("%i \n",nums[4]);
+
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%i \n",nums[i]);
+    }
+
+}
---
+++ 03_arrays/06.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main()
+{
+    int i, j, m, n;
+    int A[m][n];
+    for (i = 0; i < m; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            scanf("%d", &A[i][j]);
+        }
+    }
+}
