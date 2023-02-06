5616e24cc19fea610ff6b46bc1c3de038711f190
kelvinokumu <oumakelvinokumu.gmail.com>
2023-02-06 08:33:13 +0300

    EQUALITY

---
+++ 01_basics/07_equality.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void main()
+{
+    int b = 20, c = 30, d;
+
+    printf("Equal to \n");
+    d = c == b;
+    printf("%i \n\n", d);
+
+    printf("Not equal to \n");
+    d = c != b;
+    printf("%i \n\n", d);
+
+}
