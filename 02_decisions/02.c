18cc5eeee5f9dec1f47fa48543bb8091b7e999dc
kelvinokumu <oumakelvinokumu.gmail.com>
2023-02-08 08:19:54 +0300

    conditions

---
+++ 02_decisions/02.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void main()
+{
+    char grade = 'C';
+    // if else statement
+    if (grade == 'A')
+    {
+        printf("Good grades \n");
+    }
+    else
+    {
+        printf("Bad grades \n");
+    }
+}
---
+++ 02_decisions/03.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+void main()
+{
+    // if else if
+    int grade;
+    printf("Enter your grade \n");
+    scanf("%i",&grade);
+    if (grade > 80)
+    {
+        printf("A \n");
+    }
+    else if(grade > 70)
+    {
+        printf("B \n");
+    }
+    else if(grade > 60)
+    {
+        printf("C \n");
+    }
+    else if(grade > 50)
+    {
+        printf("D \n");
+    }
+    else{
+        printf("Fail \n");
+    }
+}
