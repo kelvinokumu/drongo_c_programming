c1b84f378dd5bfc441d0525e22f9fe62935f9ac0
kelvinokumu <oumakelvinokumu.gmail.com>
2023-02-09 12:05:19 +0300

    pointers

---
+++ 06_pointers/03.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+void main(){
+    
+
+    int a = 45;
+    int y; 
+    
+    y = a;
+
+    printf("values of a %i \n", a);
+    printf("values of y  %i \n", y);
+
+    y = 75;
+    printf("\n\n");
+
+    printf("values of a  %i \n", a);
+    printf("values of y  %i \n", y);
+
+
+}
