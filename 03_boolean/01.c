7d012974e2982e8429a6e9b6a2918ec378f7075c
kelvinokumu <oumakelvinokumu.gmail.com>
2023-02-13 08:23:56 +0300

    boolean

---
+++ 03_boolean/01.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+void main()
+{
+    // bool access = true;
+    // bool flag = true;
+    bool x;
+
+    int a = 115;
+    int b = 25;
+    x = (a < b);
+    if (x)
+    {
+        printf("this is True \n");
+    }
+    else
+    {
+        printf("this is False \n");
+    }
+}
