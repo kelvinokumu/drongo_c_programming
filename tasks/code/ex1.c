9aabf2242357d1d0c6c5004f2c01362249ac52b4
kelvinokumu <oumakelvinokumu.gmail.com>
2023-02-17 11:53:21 +0300

    ex1

---
+++ tasks/code/ex1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main()
+{
+    int number, answer;
+    printf("Enter a number to calculate its square : ");
+    scanf("%i", &number);
+    answer = number * number;
+    printf("The square is %i \n", answer);
+}
+
+void main()
+{
+}
